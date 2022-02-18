07642bf05aef3a75149ed92041e08c486e2094ef
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-18 22:39:18 +0300

    add list of users

--- user.py
+++ user.py
@@ -3,3 +3,6 @@ class User:
     """generate an instance of a class
     """
 use_list=[]#list of users
+def __init__(self,username,password):
+    self.username = username
+    self.password =password
