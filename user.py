3ff1603359f1b17ad685e63641c25af199c2b7b6
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-19 01:22:08 +0300

    save multiple users

--- user.py
+++ user.py
@@ -2,12 +2,13 @@
 class User:
     """generate an instance of a class
     """
-use_list=[]#list of users
-def __init__(self,username,password):
-    self.username = username
-    self.password =password
-    ##save users
+    user_list=[]#list of users
+    def __init__(self,username,password):
+        self.username = username
+        self.password =password
+        ##save users
     def save_user(self):
         User.user_list.append(self)
-    def delete_user(self):
-        User.user_list.remove(self)
+   
+    # def delete_user(self):
+    #     User.user_list.remove(self)
--- user_test.py
+++ user_test.py
@@ -1,22 +1,16 @@
 #!usr/bin/env python3.9
 import unittest #import unittestmodule
 from user import User #import the user class
-class User(unittest.TestCase):
+class TestUser(unittest.TestCase):
     def setUp(self):
         '''
         method to run before each test
         '''
         self.new_user=User("EsauKip","#Youcannotfind5") #new User created
-
-    def tearDown(self):
-        '''
-        clean up after each test to prevent errors
-        '''
-        User.user_list = []
     def test__init(self):
        
-        self.assertEqual(self.new_user.username, "EsauKip")
-        self.assertEqual(self.new_user.password, "#Youcannotfind5")
+        self.assertEqual(self.new_user.username,"EsauKip")
+        self.assertEqual(self.new_user.password,"#Youcannotfind5")
         ##second test
     def test_save_user(self):
         '''
@@ -25,15 +19,16 @@ class User(unittest.TestCase):
         '''
         self.new_user.save_user()
         self.assertEqual(len(User.user_list), 1)
+    def tearDown(self):
+        '''
+        clean up after each test to prevent errors
+        '''
+        User.user_list = []
         #save many users
-    def test_save_mutliple_users(self):
-        '''
-        check whether you can store more than one user
-        '''
-        self.new_user.save_user()
-        test_user = User("test", "password")
-        test_user.save_user()
-        self.assertEqual(len(User.user_list), 2)
- 
+    def test_save_multiple_users(self):
+       self.new_user.save_user()
+       test_user = User("test","trickysana")
+       test_user.save_user()
+       self.assertEqual(len(User.user_list),2)
 if __name__ == "__main__":
     unittest.main()
