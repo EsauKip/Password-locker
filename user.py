09a77986da2b1c3ab56824ba9e3f039b573ffdfe
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-18 22:45:12 +0300

    save multiple users

--- user.py
+++ user.py
@@ -6,3 +6,6 @@ use_list=[]#list of users
 def __init__(self,username,password):
     self.username = username
     self.password =password
+    ##save users
+    def save_user(self):
+        User.use_list.append(self)
