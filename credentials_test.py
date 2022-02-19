d7a92682283597ad2114086ba4bb07fcc2001aae
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-19 16:09:26 +0300

    run 6th test

--- credentials_test.py
+++ credentials_test.py
@@ -4,11 +4,13 @@ from credentials import Credentials
 class TestCredentials(unittest.TestCase):
     def setUp(self):
         self.new_cred = Credentials("Gmail", "kipronoesau28@gmail.com", "kiprono2022")
-
+#6th test
     def test_init(self):
         '''
         check if instances initialize as expected
         '''
         self.assertEqual(self.new_cred.account, "Gmail")
         self.assertEqual(self.new_cred.email, "kipronoesau28@gmail.com")
-        self.assertEqual(self.new_cred.passlock, "kiprono2022")
+        self.assertEqual(self.new_cred.password, "kiprono2022")
+if __name__ == "__main__":
+    unittest.main()
