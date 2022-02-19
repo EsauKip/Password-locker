56b762dee20549e77746f84afcd84038922cffbb
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-19 15:52:12 +0300

    import unittest

--- credentials_test.py
+++ credentials_test.py
@@ -1,1 +1,5 @@
-
+import unittest#import unittest module
+from credentials import Credentials
+class TestCredentials(unittest.TestCase):
+    def setUp(self):
+        self.new_cred = Credentials("Gmail", "kipronoesau28@gmail.com", "kiprono2022")
