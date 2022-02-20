6fe4cc91d83503b664edc605901eeef80442297b
EsauKip <esau.kiprono@student.moringaschool.com>
2022-02-20 09:36:22 +0300

    import methods

--- run.py
+++ run.py
@@ -2,7 +2,7 @@
 from credentials import Credentials
 from user import User
 import random
-# create user accounts
+  # create user accounts
 def create_useraccount(username, password):
     '''
     method creates a user account
@@ -23,3 +23,58 @@ def save_credentials(credentials):
     '''
 
     credentials.save_credentials
+    ### find user using username
+
+def find_user(username):
+    '''
+    method for find user using username
+    '''
+    return User.find_user(username)
+         # create credentials
+def create_credentials(account, email, passcode):
+    '''
+    method credentials details
+    '''
+    new_credentials = Credentials(account, email, passcode)
+    return new_credentials
+####save_credentials
+def save_credentials(credentials):
+    '''
+    save credentials
+    '''
+    credentials.save_credentials()
+##Display all the saved credentials
+def display_credentials():
+    '''
+    method to display all the saved credentials
+    '''
+    return Credentials.display_credentials()
+    #search for the account credentials
+
+def find_account(account):
+    '''
+    method to search for an account
+    '''
+    return Credentials.find_account(account)
+##delete the account credentials
+def delete_credentials(account):
+    '''
+    method to delete account
+    '''
+    account.delete_credentials()
+
+
+
+
+def main():
+    # Dealing user class first
+    print("Welcome to Password Locker! Please enter your name:  ")
+    name = input ()
+    print(f"{name}, Sign up to continue")
+    print('\n')
+    print("*" * 80)
+    print("Reply with  : cc - Sign Up,  ex -exit ")
+    print("*" * 80)
+if __name__=='__main__':
+  main()
+    
